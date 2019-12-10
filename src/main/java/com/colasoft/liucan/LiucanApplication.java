b9f76680fda71187d5f00333c315f2860281dbe5
liucan <liucan@cd.tuan800.com>
2019-12-10 22:52:46 +0800

    第一次提交代码

---
+++ src/main/java/com/colasoft/liucan/LiucanApplication.java
@@ -0,0 +1,13 @@
+package com.colasoft.liucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiucanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiucanApplication.class, args);
+    }
+
+}
