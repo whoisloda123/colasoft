210df5881492cb24d2735db68dc7c146412977ac
liucan <553670590@qq.com>
2019-12-12 14:09:04 +0800

    提交代码

--- src/test/java/com/colasoft/liucan/ApplicationTests.java
+++ src/test/java/com/colasoft/liucan/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LiucanApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
